d29b14c2164216c64b83a7aa7a4f55fed91a230a
wennshow <vladyr55555@gmail.com>
2024-08-06 15:22:57 +0300

    Updating BooksController

--- src/main/java/ru/wenn/springcourse/controllers/BooksController.java
+++ src/main/java/ru/wenn/springcourse/controllers/BooksController.java
@@ -1,4 +1,62 @@
 package ru.wenn.springcourse.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.wenn.springcourse.dao.BookDAO;
+import ru.wenn.springcourse.dao.PersonDAO;
+import ru.wenn.springcourse.models.Book;
+
+@Controller
+@RequestMapping("/books")
 public class BooksController {
+    private final BookDAO bookDAO;
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BooksController(BookDAO BookDAO, PersonDAO personDAO) {
+        this.bookDAO = BookDAO;
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book Book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") @Valid Book Book,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "books/new";
+
+        bookDAO.save(Book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "books/edit";
+
+        bookDAO.update(id, book);
+        return "redirect:/books";
+    }
+
 }
